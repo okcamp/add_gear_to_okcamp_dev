32d9a2d696a41c9fb4267b538455cd81a517858a
t4traw <t4traw@gmail.com>
2017-11-08 16:12:04 +0900

    Remove form

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
+// TODO ログインまわりの処理を追加する
+// localStorageにaccess_tokenが無かったらログイン画面をpopup
+// もしあったならaccess_tokenがactiveなのかどうかを検証する
+// access_tokenが無効(期限切れ)だった場合は再度ログインをする
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.sendMessage(tab.id, "Action");
 });
@@ -6,45 +11,32 @@ var params = {};
 var categories = {};
 var makers = {};
 
-// まずaccess_tokenがactiveなのかどうかを検証する
-
-// access_tokenが無効だった場合はログインをさせる
-
-// TODO 将来的にはcategoriesとmakersはlocalStorageにキャッシュデータを保存しておいて高速化
-
-// 実際のpopup.htmlに値を渡す
-var category_json = new XMLHttpRequest();
-category_json.open("get", "./categories.json", true);
-category_json.onload = function(){
-  // console.log(this.responseText);
-  var json = JSON.parse(this.responseText);
-  for (var i=0; i<json.length; i++){
-    categories[json[i]['id']] = json[i]['name_ja']
-  }
-}
-category_json.send(null);
-
-var maker_json = new XMLHttpRequest();
-maker_json.open("get", "./makers.json", true);
-maker_json.onload = function(){
-  // console.log(this.responseText);
-  var json = JSON.parse(this.responseText);
-  for (var i=0; i<json.length; i++){
-    makers[json[i]['id']] = json[i]['name_ja']
-  }
-}
-maker_json.send(null);
-
-// ここでメーカーとカテゴリーを正確じゃないにしても把握できるのか？
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse){
+
     name_arr = [request.title, request.current_color, request.current_style]
     mixed_title = name_arr.filter(function(e){return e}).join(' ')
-    params['asin'] = request.asin;
-    params['title'] = mixed_title;
-    params['weight'] = request.weight;
-    params['description'] = request.description;
-    chrome.tabs.create({"url": "popup.html" });
+
+    var XHR = new XMLHttpRequest();
+
+    XHR.addEventListener("progress", function(event) {
+      console.log('Submit data')
+    });
+    XHR.addEventListener("load", function(event) {
+      alert(event.target.responseText);
+    });
+    XHR.addEventListener("error", function(event) {
+      alert(event.target.responseText);
+      // rollbarとかで検知させられないかね
+    });
+    // ↓これ、この順番じゃないとダメなので注意
+    XHR.open("POST", "http://localhost:3000/ja/api/gear");
+    XHR.setRequestHeader('ACCESS_TOKEN', '3a956a05a4f25ae777e9fd45226f0b86');
+    XHR.setRequestHeader("Content-Type", "application/json")
+    XHR.send(JSON.stringify({
+      'asin_ja': request.asin,
+      'title_ja': mixed_title,
+      'weight': request.weight
+    }))
   }
 );
